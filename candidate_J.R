b211c3e900a8fdcc2d17ef6e263adb00a8345792
Jonas Hügel <jonas.huegel@med.uni-goettingen.de>
2023-09-06 13:36:54 +0200

    updated choose_directory function

--- candidate_J.R
+++ candidate_J.R
@@ -35,12 +35,14 @@ pacman::p_load(data.table, devtools, backports, Hmisc, tidyr,dplyr,ggplot2,plyr,
 
   ##utils::choose.dir is a windows functionality use tk on other systems
   choose_directory = function(caption = 'Select directory') {
-    if (exists('utils::choose.dir')) {
-      choose.dir(caption = caption)
-    } else {
-      tcltk::tk_choose.dir(caption = caption)
-    }
+  if(commandArgs()[[1L]] == "RStudio"){
+    rstudioapi::selectDirectory(caption = caption)
+  }else if (exists('utils::choose.dir')) {
+    choose.dir(caption = caption)
+  } else{
+    tcltk::tk_choose.dir(caption = caption)
   }
+}
 
 }
 
